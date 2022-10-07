395f490ca7ec3b0c9da27e041a92d47509d71d0c
SPIF KLA - Cedric Nageri <Cedric.Nageri@spedaginterfreight.lan>
2022-10-07 18:52:29 +0300

    added products path

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,26 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from products.models import Product
+from products.serializers import ProductSerializer
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-  model_data = Product.objects.all().order_by("?").first()
+  instance = Product.objects.all().order_by("?").first()
   data = {}
-  if model_data:
+
+  if instance:
+    data = ProductSerializer(instance).data
+  '''
+  if instance:
     data['id'] = model_data.id
     data['title'] = model_data.title
     data['content'] = model_data.content
     data['price'] = model_data.price
-    '''
+    data['sale_price'] = model_data.sale_price
+  
      The above is serialization,
      A model instance being turned into a python dictionary 
     '''
-  return JsonResponse(data)
+  return Response(data)
