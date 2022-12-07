2d88a1ddd20b8fd88305dfac04dd86f92d7ab8d8
NAGERI <onacedricnageri@gmail.com>
2022-12-07 12:10:28 +0300

    using Mixins

--- py-client/create.py
+++ py-client/create.py
@@ -3,7 +3,7 @@ import requests
 endpoint = "http://127.0.0.1:8000/api/products/"
 
 data = {
-  "title":"New Object creation done!"
+  "title":"Mixin Object creation done!"
 }
 get_res = requests.post(endpoint, json=data)
 print(get_res.json())
--- py-client/detail.py
+++ py-client/detail.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = "http://127.0.0.1:8000/api/products/1/"
+endpoint = "http://127.0.0.1:8000/api/products/4/"
 
 get_res = requests.get(endpoint)
 print(get_res.json())
