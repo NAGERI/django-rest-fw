84708fc99dd48ac0e6d0c01bf08e7413113e5ec3
NAGERI <onacedricnageri@gmail.com>
2022-12-13 15:18:46 +0300

    Token -  Auth

--- py-client/create.py
+++ py-client/create.py
@@ -1,9 +1,12 @@
 import requests
 
+headers = {
+    "Authorization": "Bearer 43ca8ee5713eaef2a473e17a3c6b1874b8a3a535"
+  }
 endpoint = "http://127.0.0.1:8000/api/products/"
 
 data = {
-  "title":"Mixin Object creation done!"
+  "title":"Token Object creation done!"
 }
-get_res = requests.post(endpoint, json=data)
+get_res = requests.post(endpoint, json=data,headers=headers)
 print(get_res.json())
